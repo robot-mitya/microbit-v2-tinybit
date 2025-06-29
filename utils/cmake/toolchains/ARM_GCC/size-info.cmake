4cab6150f1549c513d7c17486707d2a43a2d024b
DmitryDzz <DzakhovDS@gmail.com>
2025-06-29 21:02:59 +0700

    Fixed cmake warnings

--- utils/cmake/toolchains/ARM_GCC/size-info.cmake
+++ utils/cmake/toolchains/ARM_GCC/size-info.cmake
@@ -1,6 +1,8 @@
+#cmake_policy(SET CMP0175 NEW)
 add_custom_command(
     TARGET ${device.device}
+    POST_BUILD
     COMMAND "${ARM_NONE_EABI_SIZE}" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${device.device}"
-    DEPENDS  ${device.device}
+#    DEPENDS  ${device.device}
     COMMENT "Print total size info:"
 )
